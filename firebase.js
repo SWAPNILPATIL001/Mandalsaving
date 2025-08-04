afdb1e3da6aefea45017948f9b07cb0bb63d9ac3
SWAPNIL PATIL <160451049+SWAPNILPATIL001@users.noreply.github.com>
2025-08-04 18:54:02 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,28 +1,17 @@
 // firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  sendEmailVerification,
-  onAuthStateChanged,
-  signOut
-} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
-import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
 
-// Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCMzIzcShaNZrAtmxAE-_sY2JQuXn_zOdg",
   authDomain: "mandalsaving.firebaseapp.com",
   projectId: "mandalsaving",
-  storageBucket: "mandalsaving.firebasestorage.app",
+  storageBucket: "mandalsaving.appspot.com",
   messagingSenderId: "383737474637",
-  appId: "1:383737474637:web:27a89d42deaf12337b7604",
-  measurementId: "G-20NVPLXBK2"
+  appId: "1:383737474637:web:27a89d42deaf12337b7604"
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
 
-export { auth, db, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, onAuthStateChanged, signOut, doc, setDoc, getDoc };
+export { auth };
